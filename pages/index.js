e67b4604cffc5924bf534eeff6951a1a1d77dc2a
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-25 08:13:22 -0300

    Running the pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Logo } from './../components'
 
 export default function Home() {
   return (
     <div>
-      
+      <Logo />
     </div>
   )
 }
