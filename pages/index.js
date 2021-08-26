0031de44620bc8d0c5b468808e839f89d7fb1bf2
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-25 21:05:15 -0300

    First page on road

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,88 @@
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+
+import { 
+  Container, 
+  Box, 
+  Input, 
+  Button, 
+  Text, 
+  FormControl, 
+  FormLabel, 
+  FormHelperText } from '@chakra-ui/react'
+
+
 import { Logo } from './../components'
 
+let schema = yup.object().shape({
+  name: yup.string().required(),
+  age: yup.number().required().positive().integer(),
+  email: yup.string().email(),
+  website: yup.string().url(),
+  createdOn: yup.date().default(function () {
+    return new Date();
+  }),
+});
+
+const validationSchema = yup.object().shape({
+  email: yup.string().email('E-mail inválido').required('Preenchimento obrigatório'),
+  password: yup.string().required('Preenchimento obrigatório'),
+  username: yup.string().required('Preenchimento obrigatório'),
+})
+
 export default function Home() {
+  const formik = useFormik({
+    onSubmit: () => {},
+    validationSchema,
+    initialValues: {
+      email: '',
+      username: '',
+      password: ''
+    },
+  })
   return (
-    <div>
+    <Container p={4} centerContent>
       <Logo />
-    </div>
+
+      <Box p={4} mt={8}>
+
+        <Text>Faça sua agenda automatizada</Text>
+
+      </Box>
+
+      <Box>
+
+        <FormControl id='email' p={4} isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input type='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+          <FormHelperText textColor='#e74c3c'>{formik.errors.password}</FormHelperText>
+        </FormControl>
+
+        <FormControl id='password' p={4} isRequired>
+          <FormLabel>Senha</FormLabel>
+          <Input type='password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+          <FormHelperText textColor='#e74c3c'>{formik.errors.password}</FormHelperText>
+        </FormControl>
+
+        <Box display='flex' flexDirection='row' alignItems='center'>
+
+          <Text>clocker.com</Text>
+          <FormControl id='username' p={4} isRequired>
+            <Input type='username' value={formik.values.username} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+          <FormHelperText textColor='#e74c3c'>{formik.errors.username}</FormHelperText>
+          </FormControl>
+
+        </Box>
+
+        <Box p={4}>
+
+          <Button width='100%'>Seguir</Button>
+
+        </Box>
+
+
+      </Box>
+    </Container>
+    
   )
 }
