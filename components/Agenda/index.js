7883d96afb40deb81663a83cd0adb12b115d5beb
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-28 21:38:56 -0300

    Authentication done!

--- components/Agenda/index.js
+++ components/Agenda/index.js
@@ -1,8 +1,11 @@
+import { Button } from '@chakra-ui/react'
+import firebase from './../../config/firebase'
 
 export const Agenda = () => {
+  const logout = () => firebase.auth().signOut();
     return (
       <div>
-          Agenda
+          <Button>Sair</Button>
       </div>
     )
   }
