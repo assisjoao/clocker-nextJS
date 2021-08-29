bd5530b10a2ffe225f767094b43213a619c225c0
João Carlos Silva de Assis <joaocarlosdeassis@poli.ufrj.br>
2021-08-29 11:34:42 -0300

    Update firebase server config

--- config/firebase/server.js
+++ config/firebase/server.js
@@ -1,27 +1,3 @@
-// //import { initializeApp } from "firebase/app";
-// import firebaseClient from "firebase/app";
-// import 'firebase/auth'
-
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_APP_ID,
-//     measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-//   };
-
-
-// //const app = initializeApp(firebaseConfig);
-
-  
-// const app = firebaseClient.apps.length ? firebaseClient.app() : firebaseClient.initializeApp(firebaseConfig)
-
-// export const persistenceMode = firebaseClient.auth.Auth.Persistence.LOCAL
-
-// export {firebaseClient}
-
 import firebaseServer from 'firebase-firebaseServer'
 
 const app = firebase.apps.length
@@ -42,12 +18,3 @@ const app = firebase.apps.length
     })
 
 export { firebaseServer }
-
-// import admin from "firebase-admin"
-
-
-// admin.initializeApp({
-//   credential: admin.credential.cert({
-//    type: process.env.TYPE,
-//    })
-// });
